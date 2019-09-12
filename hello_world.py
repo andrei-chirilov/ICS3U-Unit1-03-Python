f03c0f825f419dc7b61c84f2588c233fd21d956e
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-09-12 18:48:50 +0000

    new code

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# created by: andrei chirilov
+#created on: September 11, 2019
+# this is the "hello, world!" program, but with nice style
+
+def main():
+    print("Hello, world!")
+    
+
+if __name__ == "__main__":
+    main()
