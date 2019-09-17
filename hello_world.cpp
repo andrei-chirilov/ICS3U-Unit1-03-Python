44c947a4280da96a0400dfaffb4bec15078e05c0
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-09-17 23:33:51 +0000

    commit message

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// copyright(c) 2019 Andrei All rights reserved.
+//
+// created by: Andrei Chirilov
+// created on: september 2019
+// this is the "Hello, world!" program, but with nice style
+
+# include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+}
--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 # created by: andrei chirilov
-#created on: September 11, 2019
+# created on: September 11, 2019
 # this is the "hello, world!" program, but with nice style
+
 
 def main():
     print("Hello, world!")
-    
+
 
 if __name__ == "__main__":
     main()
